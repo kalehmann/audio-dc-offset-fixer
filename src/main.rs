212c916fd411f7e417ff7a68241a58516d6168bc
Karsten Lehmann <mail@kalehmann.de>
2024-11-25 23:58:03 +0100

    feat: Switch to rolling average

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,47 @@ use clap::{Parser};
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(value_name = "FILE")]
+    #[arg(value_name = "IN_FILE")]
     in_file: PathBuf,
 
-    #[arg(value_name = "FILE")]
+    #[arg(value_name = "OUT_FILE")]
     out_file: PathBuf,
+
+    #[arg(short, long, value_name = "N", default_value_t = 1000)]
+    samples_to_consider: usize,
 }
 
 fn main() {
     let cli = Cli::parse();
     let mut reader = hound::WavReader::open(cli.in_file).unwrap();
-    let len = reader.len();
     let spec = reader.spec();
     let mut writer = hound::WavWriter::create(cli.out_file, spec).unwrap();
 
-    println!("WAV channels: {}", spec.channels);
-    println!("WAV sample rate: {}", spec.sample_rate);
-    println!("WAV bits per sample: {}", spec.bits_per_sample);
+    let mut rolling_avg = 0.0;
+    let center = cli.samples_to_consider / 2;
+    let mut samples_for_avg = vec![0.0; cli.samples_to_consider];
 
-    let mut min: f64 = 0.0;
-    let mut max: f64 = 0.0;
-    let mut avg: f64 = 0.0;
-    for s in reader.samples::<i16>() {
+    for (i, s) in reader.samples::<i16>().enumerate() {
 	let sample = s.unwrap() as f64;
-	if sample > max {
-	    max = sample;
+	let write_index = i % cli.samples_to_consider;
+	let read_index = (i + center) % cli.samples_to_consider;
+	let sample_to_remove = samples_for_avg[write_index];
+	rolling_avg -= sample_to_remove / cli.samples_to_consider as f64;
+	samples_for_avg[write_index] = sample;
+	rolling_avg += sample / cli.samples_to_consider as f64;
+	if i < center {
+	    continue;
+	} else if i == cli.samples_to_consider {
+	    for j in 0..center {
+		let new_sample = (samples_for_avg[j] - rolling_avg) as i16;
+		writer.write_sample(new_sample).unwrap();
+	    }
 	}
-	if sample < min {
-	    min = sample;
-	}
-	avg += sample / len as f64
+	let new_sample = (samples_for_avg[read_index] - rolling_avg) as i16;
+	writer.write_sample(new_sample).unwrap();
     }
-
-    println!("Minimum is {}", min);
-    println!("Maximum is {}", max);
-    println!("Average is {}", avg);
-
-    reader.seek(0).expect("Unable to seek the beginning of the input file");
-
-    for s in reader.samples::<i16>() {
-	let sample = s.unwrap() as f64 - avg;
-	writer.write_sample(sample as i16).unwrap();
+    for i in center + 1..cli.samples_to_consider {
+	writer.write_sample((samples_for_avg[i] - rolling_avg) as i16).unwrap();
     }
     writer.finalize().unwrap()
 }
